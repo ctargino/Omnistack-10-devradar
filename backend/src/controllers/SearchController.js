8364b47133cf2623beb43a2ed914af01b4270c65
ctargino <c.targino@gmail.com>
2020-01-20 14:24:24 -0300

    add devradar project

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,24 @@
+const Dev = require('../models/dev');
+const parseStrinAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(request, response) {
+    const { latitude, lagitude, techs } = request.query;
+    const techsArray = parseStrinAsArray(techs);
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        }
+      }
+    })
+    return response.json({ devs });
+  }
+}
